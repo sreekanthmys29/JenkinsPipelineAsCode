7649e1b2d87a215871ca149a5be71fd85b0d93b9
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-07-25 15:59:07 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,11 +1,14 @@
 def call(Map params = [:]){
   
- echo "Executing Sonar Qube "
  
- sh 'mvn verify sonar:sonar \
-     mvn clean verify sonar:sonar \
-	-Dsonar.projectKey=params.projectKey \
-	-Dsonar.host.url=params.url \
-	-Dsonar.login=params.login'
+ 
+ echo "Executing Sonar Qube analysis"
+
+  sh """
+    mvn clean verify sonar:sonar \
+      -Dsonar.projectKey=${params.projectKey} \
+      -Dsonar.host.url=${params.url} \
+      -Dsonar.login=${params.login}
+  """
 
 }
