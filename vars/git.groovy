5c27a21b0002c1262e419e4fcf39c4a9ab611e42
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-07-25 13:22:12 +0530

    Update git.groovy

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,4 +1,4 @@
-def call(String branchName,credentialsId,url){
+def call(String branchName,String credentialsId, string url){
   echo "Git Checkout -------------"
   
   git branch:"${branchName}", credentialsId:"${credentialsId}", url:"{url}"
