84ccefb086e2725bee99c0256f5112930ba4a796
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-07-25 15:39:51 +0530

    gitcone.groovy

--- vars/gitcone.groovy
+++ vars/gitcone.groovy
@@ -1,7 +1,9 @@
 def call(Map params = [:]) {
+     echo " Invoke Git"
     checkoutFromGit(params)
 }
 
 def checkoutFromGit(Map params) {
+    echo " Invoke Git"
     git branch: params.branch, credentialsId: params.credentialsId, url: params.url
 }
