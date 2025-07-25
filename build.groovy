6c0912a801679958e6e4aa1234bc42d7c81b19e7
sreekanthmys29 <110405027+sreekanthmys29@users.noreply.github.com>
2025-07-25 11:48:49 +0530

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,1 +1,18 @@
-
+def call(String goal){
+  
+  echo "Build Goal -------------${goal}"
+  
+   if(${goal} == "clean"){
+      sh 'mvn clean'
+   }
+   else if(${goal} == "install"){
+      sh 'mvn clean install'
+   }
+   else if(${goal} == "package"){
+      sh 'mvn clean package'
+   }
+  else {
+       echo "Unknown goal: ${goal}"
+                 
+   }
+}
